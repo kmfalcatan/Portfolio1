0268d14b101a44fb45eabf8dd0e4937c23d1d834
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-01-18 19:37:58 +0800

    aksdugaskdg

--- POrtfolio/script.js
+++ POrtfolio/script.js
@@ -1,3 +1,12 @@
 function toggleMenu(menuIcon) {
-    menuIcon.classList.toggle("change")
+    menuIcon.classList.toggle('change');
+    var navigationContainer = document.querySelector('.navigationContainer');
+    
+    // Check if the viewport width is less than 768px (tablet mode)
+    if (window.innerWidth < 768) {
+        navigationContainer.style.display = (navigationContainer.style.display === 'flex') ? 'none' : 'flex';
+    } else {
+        // If not in tablet mode, always show the navigationContainer
+        navigationContainer.style.display = 'flex';
+    }
 }
